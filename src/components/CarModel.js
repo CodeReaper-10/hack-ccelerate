3357dfd66096e0b2f9768faa28650ffc13f8772f
Swapnil Jain <swapniljain240@gmail.com>
2023-02-04 22:36:20 +0530

    firebase and carModel

---
+++ src/components/CarModel.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function CarModel() {
+  return (
+    <div className="border-2 border-white">
+      
+    </div>
+  )
+}
--- src/components/Container.js
+++ src/components/Container.js
@@ -1,13 +1,14 @@
-import React, { useState } from 'react'
-import Map from './Map'
-import ServiceItems from './ServiceItems'
+import React, { useState } from "react";
+import CarModel from "./CarModel";
+import Map from "./Map";
+import ServiceItems from "./ServiceItems";
 
 export default function Container() {
-   const[service,setService] = useState(true);
   return (
-    <div className='grid grid-cols-3 p-5'>
-      {service?<ServiceItems className="w-1/3 "/>:""}
-        <Map className="w-2/3"/>
-        </div>
-  )
+    <div className="grid grid-cols-3 p-5">
+      <ServiceItems />
+      <Map />
+      <CarModel />
+    </div>
+  );
 }
